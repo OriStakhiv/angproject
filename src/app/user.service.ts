614a0c3a52ff647eaafbbec641229623e654b751
Ori <orysiastakh@gmail.com>
2018-12-12 13:15:30 +0200

    add login html component

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from "./model/user.model";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'http://localhost:8080/user-portal/users';
 
-  constructor() { }
+  getUsers() {
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
+  getUserById(id: number) {
+    return this.http.get<User>(this.baseUrl + '/' + id);
+  }
+
+  createUser(user: User) {
+    return this.http.post(this.baseUrl, user);
+  }
+
+  updateUser(user: User) {
+    return this.http.put(this.baseUrl + '/' + user.id, user);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(this.baseUrl + '/' + id);
+  }
 }
